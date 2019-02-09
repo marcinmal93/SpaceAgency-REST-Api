34aa63f5213e86028806569449c4509f7490e792
marcinmal93 <doubleM93@gmail.com>
2019-02-09 14:05:20 +0200

    Revert "Initial commit"
    This reverts commit e187e1e3454b61c1c7bb802b003e01d1b9262a97.

--- SpaceAgency/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SpaceAgency.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- SpaceAgency/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SpaceAgency.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
